249ee3559cd19c01315756344960c6d5a09291af
soanja <soanja.mirindra.johanna.ravalisaona@efrei.net>
2022-10-24 17:51:16 +0200

    upload student class

---
+++ src/efrei/fr/domain/Student.java
@@ -0,0 +1,21 @@
+package efrei.fr.domain;
+
+public class Student {
+
+    private String lastName;
+    private String firstName;
+    private String phone;
+    private String email;
+    private int yearRegistered;
+    private String studentNumber;
+
+
+    public double averageGPA(){
+        return 4;
+    }
+
+    public void listSubjectsWithNoGrade(){
+        System.out.println("this subjects");
+    }
+
+}
